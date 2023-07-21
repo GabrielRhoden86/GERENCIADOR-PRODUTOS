008c0613496234fbb2de78c4420d7acff625e323
Gabriel Rhoden <gabrielrhodden@gmail.com>
2023-07-21 12:44:59 -0300

    inserida a documentação Swaager

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(title="Gerenciador de Produtos - API", version="1.0")
+ */
+
 use App\Models\Produto;
 use App\Models\Usuario;
 use App\Services\ValidaProdutosApi;
@@ -9,6 +13,17 @@ use App\Services\ValidaUsuarioApi;
 
 class ApiController extends Controller
 {
+
+    /**
+     * @OA\Get(
+     *     path="/api/produtos",
+     *     @OA\Response(response="200", description="Lista de produtos"),
+     *     externalDocs={
+     *         "description": "Link para o endpoint",
+     *         "url": "http://localhost:8000/api/produtos"
+     *     }
+     * )
+     */
 
     public function showProdutos()
     {
@@ -31,6 +46,23 @@ class ApiController extends Controller
         }
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/usuario/{id}",
+     *     @OA\Response(response="200", description="Detalhes do usuário"),
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID do usuário",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\ExternalDocumentation(
+     *         description="Link para o endpoint",
+     *         url="http://localhost:8000/api/usuario/{id}"
+     *     )
+     * )
+     */
     public function showUsuario($id)
     {
         $usuarios = Usuario::with('produto.categoria')->where('id', $id)->first();
@@ -38,7 +70,7 @@ class ApiController extends Controller
         // Valide os dados antes de enviá-los ao cliente
         if ($usuarios) {
 
-            $validator = new ValidaProdutosApi();
+            $validator = new ValidaUsuarioApi();
             foreach ($usuarios as $usuario) {
 
                 if (is_object($usuario)) {
