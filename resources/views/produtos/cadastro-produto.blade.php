f471074858f6dbff93d9b476754c778a7c3bcb44
Gabriel Rhoden <gabrielrhodden@gmail.com>
2023-07-22 13:41:43 -0300

    correção bugs 2

--- resources/views/produtos/cadastro-produto.blade.php
+++ resources/views/produtos/cadastro-produto.blade.php
@@ -10,20 +10,25 @@
 
             {{ session('msg') == false }}
         @endif
+        @if (count($categorias) < 1)
+            <div class="alert alert-info">
+                <p>Antes de cadastrar um produto é necessário cadastrar uma categoria!</p>
+            </div>
+        @endif
         <h2 class="pt-5 text-dark mt-4">Cadastrar Produto</h2>
         <div class="container mt-5 p-2 d-flex justify-content-center">
             <form action="/produtos/cadastro-produto" method="POST" enctype="multipart/form-data" class="col-md-7">
                 @csrf
                 <div class="form-group">
-                    <input type="text" name="nome" class="form-control  " id="nome"
-                        placeholder="Digite o nome do produto">
+                    <input required type="text" name="nome" class="form-control  "
+                        id="nome"placeholder="Digite o nome do produto">
                 </div>
                 <div class="form-group">
-                    <input type="number" step="0.01" name="valor" class="form-control" id="valor"
-                        placeholder="Digite o valor do produto">
+                    <input required type="number" step="0.01" name="valor" class="form-control"
+                        id="valor"placeholder="Digite o valor do produto">
                 </div>
                 <div class="form-group">
-                    <select class="form-control" id="categoria" name="categoria_id">
+                    <select required class="form-control" id="categoria" name="categoria_id">
                         <option>Selecione uma categoria</option>
                         @foreach ($categorias as $categoria)
                             <option value={{ $categoria->id }}>{{ $categoria->nome }}</option>
@@ -31,8 +36,10 @@
                     </select>
 
                 </div>
-                <button type="submit" class="btn btn-primary mt-2">Cadastrar</button>
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
             </form>
+
+
 
         </div>
     @endsection
